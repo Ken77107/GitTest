02ac444697c7caa33445f74f2bd96acf87d4004f
ivanyen <ivan.chen@ehsn.com.tw>
2018-09-21 14:10:38 +0800

    testˇ

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"viewDidLoad");
-    NSLog(@"viewDidLoad2");
+    NSLog(@"viewDidLoad3");
 }
 
 
